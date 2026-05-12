93ed5613ecc6604877232f31754975895fcd1106
agomemart <agomemart@ieschandomonte.edu.es>
2026-05-12 14:29:03 +0200

    v.6.0.4

---
+++ src/ud6/empresa/ControladorEmpresa.java
@@ -0,0 +1,37 @@
+package ud6.empresa;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class ControladorEmpresa {
+
+    @FXML
+    private ListView<?> lstEmpresas;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    private TextField txtWeb;
+
+    @FXML
+    void actualizar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void agregar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void borrar(ActionEvent event) {
+
+    }
+
+}
