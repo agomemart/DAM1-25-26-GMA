41a5287e64b11f0f24214002b94c730a14f4827c
agomemart <agomemart@ieschandomonte.edu.es>
2026-05-14 18:07:59 +0200

    v.6.0.6-rev

--- src/ud6/empresa/ControladorEmpresa.java
+++ src/ud6/empresa/ControladorEmpresa.java
@@ -1,12 +1,16 @@
 package ud6.empresa;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -15,7 +19,7 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 
-public class ControladorEmpresa implements Initializable{
+public class ControladorEmpresa implements Initializable {
 
     @FXML
     private ListView<Empresa> lstEmpresas;
@@ -34,9 +38,17 @@ public class ControladorEmpresa implements Initializable{
 
     @FXML
     void actualizar(ActionEvent event) {
-        Empresa e = new Empresa(Integer.parseInt(txtId.getText()));
-        int i = lstEmpresas.getItems().indexOf(e);
-        Empresa eL = lstEmpresas.getItems().get(i);
+        int id = Integer.parseInt(txtId.getText());
+        
+        Empresa buscada = new Empresa(id);
+        int i = lstEmpresas.getItems().indexOf(buscada);
+
+        if (i != -1) {
+            Empresa nueva = new Empresa(id, txtNombre.getText(), txtWeb.getText());
+
+            lstEmpresas.getItems().set(i, nueva);
+        }
+
     }
 
     @FXML
@@ -58,19 +70,45 @@ public class ControladorEmpresa implements Initializable{
 
     @FXML
     void abrir(ActionEvent event) {
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Abrir archivo");
+
+        File file = chooser.showOpenDialog(null);
+
+        if (file == null) {
+            return;
+        }
+
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            lstEmpresas.getItems().clear();
+
+            List<String> lineas = in.readAllLines();
+            for (String linea : lineas) {
+                int posPrimerEspacio = linea.indexOf(' ');
+                int id = Integer.parseInt(linea.substring(0, posPrimerEspacio));
+                String nombre = linea.substring(posPrimerEspacio + 2);
+                lstEmpresas.getItems().add(new Empresa(id, nombre, null));
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error de I/O");
+        }
 
     }
 
     @FXML
     void guardar(ActionEvent event) {
         FileChooser chooser = new FileChooser();
-        chooser.setTitle("Guardar");
-        chooser.setInitialDirectory(null);
+        chooser.setTitle("Guardar archivo");
         chooser.setInitialFileName("empresas.txt");
 
         File file = chooser.showSaveDialog(null);
 
-        try (BufferedWriter out = new BufferedWriter(new FileWriter("empresas.txt"))) {
+        if (file == null) {
+            return;
+        }
+
+        try (BufferedWriter out = new BufferedWriter(new FileWriter(file))) {
             for (Empresa e : lstEmpresas.getItems()) {
                 out.write(e.toString());
                 out.newLine();
@@ -78,7 +116,12 @@ public class ControladorEmpresa implements Initializable{
         } catch (IOException e) {
             System.out.println("Error de I/O");
         }
-        
+
+    }
+
+    @FXML
+    void cerrar(ActionEvent event) {
+        Platform.exit();
     }
 
 }
